5056ff1af250934b5ebe373bd84f04c115da312d
Tonatiuh Gomez <tonatiuh.gom@gmail.com>
2025-07-14 23:01:00 -0600

    feat: subscribtions starting

--- screens/MembresiasScreen/MembresiasScreen.style.tsx
+++ screens/MembresiasScreen/MembresiasScreen.style.tsx
@@ -5,11 +5,12 @@ const MembresiasScreenStyles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: "#121212",
   },
   text: {
     fontSize: 20,
     fontWeight: "bold",
+    color: "#fff",
   },
 });
 
